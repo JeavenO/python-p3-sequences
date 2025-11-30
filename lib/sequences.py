d42d3c56f0c871320e17ea207e8a4843d94ea02d
Jeaven Onyango <jeaveno123@gmail.com>
2025-11-30 13:24:29 +0300

    Add print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+
+    if n == 1:
+        print([0])
+        return
+
+    fib = [0, 1]
+
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+
+    print(fib)
